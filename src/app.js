eb96a18bc524ad8074d4eaf6dcb47803c988f341
antoinelc7 <antoine.lecorre@yahoo.fr>
2024-01-25 15:44:57 +0100

    Implement registration and auth logic for the API

--- src/app.js
+++ src/app.js
@@ -4,10 +4,15 @@ const port = '3000';
 
 const server = express();
 
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://mongo/apinode");
+
+server.use(express.json());
+server.use(express.urlencoded());
+
+const userRoute = require("./api/routes/userRoute");
+userRoute(server);
+
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-server.get('/', (req, res) => {
-    res.send('Hello World!');
 });
