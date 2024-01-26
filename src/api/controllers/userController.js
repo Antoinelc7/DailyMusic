1f479c2f5a8918128f9e04db20e56850c3dae0dd
antoinelc7 <antoine.lecorre@yahoo.fr>
2024-01-26 11:33:14 +0100

    Added bcrypt + hash logic for password in register and login

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -1,16 +1,24 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 exports.userRegister = async (req, res) => {
     try {
-        let newUser = new User(req.body);
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        let newUser = new User({
+            ...req.body,
+            password: hashedPassword
+        });
+
         let user = await newUser.save();
         res.status(201).json({message: `Utilisateur crée: ${user.email}`})
     } catch (error) {
         console.log(error);
         res.status(401).json({message: "Requête invalide"});
     }
-}
+};
 
 exports.userLogin = async (req, res) => {
     try {
@@ -20,18 +28,20 @@ exports.userLogin = async (req, res) => {
             res.status(500).json({message: "utilisateur non trouvé"});
             return;
         }
-        if(user.email === req.body.email && user.password === req.body.password) {
-            const userData = {
-                id: user._id, 
-                email: user.email,
-                role: true
-            };
-            const token = await jwt.sign(userData, process.env.JWT_KEY, { expiresIn: "10h"});
-            res.status(200).json({token});
 
-        } else {
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if(!validPassword) {
             res.status(401).json({message: "Email ou mot de passe incorrect"});
+            return;
         }
+
+        const userData = {
+            id: user._id, 
+            email: user.email,
+            role: user.role
+        };
+        const token = await jwt.sign(userData, process.env.JWT_KEY, { expiresIn: "10h"});
+        res.status(200).json({token});
     } catch (error) {
         console.log(error);
         res.status(500).json({message: "Une erreur s'est produite lors du traitement"});
