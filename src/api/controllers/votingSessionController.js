8f696cbb6bfb87385901f886aac7397d62253993
Arzoter <arthur.cloux@gmail.com>
2024-01-28 15:05:58 +0100

    feat: added delteSession method & reviewed routes

--- src/api/controllers/votingSessionController.js
+++ src/api/controllers/votingSessionController.js
@@ -15,22 +15,36 @@ exports.createSession = async (req, res) => {
     }
 };
 
-exports.getSession = async (req, res) => {
-    try {
-        let session = await VotingSession.findById(req.params.sessionId);
-        res.status(200).json(session);
-    } catch (error) {
-        console.log(error);
-        res.status(404).json({message: "Session introuvable"});
-    }
-};
-
 exports.getAllSessions = async (req, res) => {
     try {
         let sessions = await VotingSession.find();
         res.status(200).json(sessions);
     } catch (error) {
         console.log(error);
-        res.status(404).json({message: "Sessions introuvables"});
+        res.status(404).json({message: "Sessions introuvables.."});
     }
-}
+};
+
+exports.getSession = async (req, res) => {
+    try {
+        let session = await VotingSession.findById(req.params.sessionId);
+        res.status(200).json(session);
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({message: "Session introuvable.."});
+    }
+};
+
+exports.deleteSessions = async (req, res) => {
+    try {
+        const deletedSession = await VotingSession.findByIdAndDelete(req.params.sessionId);
+        if (!deletedSession) {
+            res.status(404).json({ message: "Session introuvable.." });
+            return;
+        }
+        res.status(204).json({ message: "Session supprimée avec succès!" })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Erreur serveur." });
+    }
+};
--- src/api/routes/votingSessionRoute.js
+++ src/api/routes/votingSessionRoute.js
@@ -1,7 +1,7 @@
 module.exports = (server) => {
     const auth = require("../middlewares/auth");
     const votingSessionController = require("../controllers/votingSessionController");
-    server.post("/votingSession/create-session", auth, votingSessionController.createSession);
-    server.get("/votingSession/get-session/:sessionId", auth, votingSessionController.getSession);
-    //server.get("/votingSession/get-all-sessions", votingSessionController.getAllSessions);
+    server.post("/votingSession", auth, votingSessionController.createSession);
+    server.get("/votingSession/", votingSessionController.getAllSessions);
+    server.get("/votingSession/:sessionId", auth, votingSessionController.getSession);
 }
